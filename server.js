8c975b3dbfcacbb3d3a03f0698c5faa4855696ae
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-06-23 02:03:51 -0300

    again

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.render('index')
 })
 
