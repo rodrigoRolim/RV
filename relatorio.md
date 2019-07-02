f582a921ad885ad90420aacc7e9dfc9cdc482095
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-07-02 02:03:27 -0300

    asd

---
+++ relatorio.md
@@ -0,0 +1,30 @@
+**Rodrigo Rolim Veras**
+**1694111**
+****
+#### Realidade Aumentada Mobile
+
+Essa aplicação de realidade aumentada foi feita em javascript na plataforma node. 
+A aplicação executa no navegador de qualquer celular com camera frontal. Para fazer tal
+aplicação foi utilizado uma biblioteca em javascript chamada de AR.js.
+
+A aplicação é servida através do servidor node.js e foi implantada no host da heroku.
+##### Código da view da aplicação
+É nessa parte do código que está configurado o uso da biblioteca AR.js, e também estão configurados o o marcador, o uso da camera para detectar o marcador e o objeto virtual gerado.
+
+![banana](html.png)
+
+##### Código do servidor
+
+Esse é código onde está configurado o servidor node, por meio do qual a aplicação será disponibilizada em uma determinada porta.
+
+![maca](node.png)
+
+##### Link de acesso à aplicação
+
+link para a [RV](https://infinite-sea-95325.herokuapp.com/ "clique aqui para ver, professor") disponiblizada no heroku.
+
+##### marcador usado
+
+![hiro](hiro.png)
+
+**Repositório do código:** [repositório]()
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const cors = require('cors'
-)
+const cors = require('cors')
 app.set("/", "html")
 app.use(cors())
 app.use(express.static(path.join(__dirname, "/")))
@@ -10,20 +9,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', `http://localhost:${process.env.PORT}`);
-
-  // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
   next();
 });
 app.get('/', (req, res) => {
@@ -31,5 +20,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(process.env.PORT, () => {
-  console.log('Listening on port 8080')
+  console.log('Listening on port')
 })
